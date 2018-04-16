3b400ce61e9de87cdfa25c267d71ca85a497521c
David Isaksson <davidisaksson93@gmail.com>
2018-04-16 22:01:26 +0200

    Added default constructor to Entity

--- src/entities/entity.cpp
+++ src/entities/entity.cpp
@@ -5,7 +5,16 @@ Entity::Entity(Graphics *graphics, const std::string &filePath,
                float posX, float posY, Vector2 speed)
     : GameObject(graphics, filePath, srcX, srcY, width, height, posX, posY),
       m_Velocity(speed),
-      m_Acceleration(Vector2({0, 0})),
+      m_Acceleration(Vector2::Zero()),
+      m_Alive(true)
+{
+
+}
+
+Entity::Entity()
+    : GameObject(),
+      m_Velocity(Vector2::Zero()),
+      m_Acceleration(Vector2::Zero()),
       m_Alive(true)
 {
 
--- src/entities/entity.h
+++ src/entities/entity.h
@@ -10,7 +10,8 @@ class Entity : public GameObject
 public:
     Entity(Graphics* graphics, const std::string& filePath,
            int srcX, int srcY, int width, int height,
-           float posX, float posY, Vector2 speed);
+           float posX, float posY, Vector2 speed = Vector2::Zero());
+    Entity();
 protected:
     Vector2 m_Velocity;
     Vector2 m_Acceleration;
